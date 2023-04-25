6ee7f9de78eda116f6efdde1dac6b5346c2cb688
7afsahatem <hafsa7hatem@gmail.com>
2023-04-24 18:25:18 -0700

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,10 +6,11 @@
 int main(void)
 {
 	char c = 'z';	
+	
 	while (c >= 'a') /* loop from z to a */
 	{
 		putchar(c); /* print the current character */
-		c--; /* decrement the characte */
+		c--; /* decrement the character */
 	}
 	putchar('\n'); /* print a new line */
 	return (0);
