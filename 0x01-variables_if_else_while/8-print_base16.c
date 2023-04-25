581b3cb798a8aeaead22a0e5b101715ce0bc94ef
7afsahatem <hafsa7hatem@gmail.com>
2023-04-24 18:05:22 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	int i; // declare an integer variable
-	char c; // declare a character variable
-	for (i = 0; i < 10; i++) // loop from 0 to 9
+	int i; /* declare an integer variable */
+	char c; /* declare a character variable */
+	for (i = 0; i < 10; i++) /* loop from 0 to 9 */
 	{
-		putchar(i + '0'); // print the current digit
+		putchar(i + '0'); /* print the current digit */
 	}
-	for (c = 'a'; c <= 'f'; c++) // loop from a to f
+	for (c = 'a'; c <= 'f'; c++) /* loop from a to f */
 	{
-		putchar(c); // print the current letter
+		putchar(c); /* print the current letter */
 	}
-	putchar('\n'); // print a new line
+	putchar('\n'); /* print a new line */
 	return (0);
 }
