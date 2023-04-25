6662e721a400e2049ebf0a5738d9aedd24c8677c
7afsahatem <hafsa7hatem@gmail.com>
2023-04-24 18:13:02 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+	while (c >= 'a') // loop from z to a
+	{
+		putchar(c); // print the current character
+		c--; // decrement the characte
+	}
+	putchar('\n'); // print a new line
+	return (0);
+}
