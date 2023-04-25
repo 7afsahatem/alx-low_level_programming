ac7e9a3acc8999f984748e98ca3f4bf58bdcf102
7afsahatem <hafsa7hatem@gmail.com>
2023-04-24 18:01:02 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i; // declare an integer variable
+	char c; // declare a character variable
+	for (i = 0; i < 10; i++) // loop from 0 to 9
+	{
+		putchar(i + '0'); // print the current digit
+	}
+	for (c = 'a'; c <= 'f'; c++) // loop from a to f
+	{
+		putchar(c); // print the current letter
+	}
+	putchar('\n'); // print a new line
+	return (0);
+}
