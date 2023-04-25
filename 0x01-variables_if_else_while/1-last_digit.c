94cbaa7ddfef8178eb97a2f7ec66d7c622add0a0
7afsahatem <hafsa7hatem@gmail.com>
2023-04-24 17:24:48 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,14 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main-program entry point.
- * Return:0 if no error, non-zero value if error.
+ * main - prints the last digit of a random number
+ * Return: 0 if no error, non-zero value if error
  */
 int main(void)
 {
 	int n;
 	int ldigit;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ldigit = n % 10;
