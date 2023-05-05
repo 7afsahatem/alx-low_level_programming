9f555834ff771c29c92d3f5584a950b10e387e2f
7afsahatem <hafsa7hatem@gmail.com>
2023-05-05 14:47:29 -0700

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int j;
-	int temp; // declare a temporary variable to store the value of a[i]
 
-	for (i = 0; i < n/2; i++) // use n/2 as the limit
+	int j;
+	for (i = 0; i < n--; i++)
 	{
-		temp = a[i]; // store the value of a[i] in temp
-		a[i] = a[n-i-1]; // swap a[i] and a[n-i-1]
-		a[n-i-1] = temp; // assign temp to a[n-i-1]
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
 	}
 }
